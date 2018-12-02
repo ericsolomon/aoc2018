d1d5df3e689639cee8b1376943d1d3e08e2888d0
Eric Solomon <eric@ericrsolomon.com>
2018-12-02 16:56:32 -0600

    day 2 nim

---
+++ day_2/day_2.nim
@@ -0,0 +1,54 @@
+import strutils, sequtils, sets
+
+proc partOne: int =
+  let input = readFile("input.txt").splitLines.filter(proc(x: string): bool = x != "")
+
+  var
+    twoCount = 0
+    threeCount = 0
+
+  for id in input:
+    var
+      foundTwo = false
+      foundThree = false
+    
+    for ch in id.deduplicate():
+      var count = id.count(ch)
+    
+      if count == 2 and not foundTwo:
+        twoCount += 1
+        foundTwo = true
+      if count == 3 and not foundThree:
+        threeCount += 1
+        foundThree = true
+
+      if foundTwo and foundThree:
+        break
+
+  result = twoCount * threeCount
+
+
+proc stringDiff(x: string, y: string): int =
+  result = -1
+  
+  for idx, ch in x:
+    if ch != y[idx]:
+      if result != -1:
+        return -1
+
+      result = idx
+
+proc partTwo: string =
+  let input = readFile("input.txt").splitLines.filter(proc(x: string): bool = x != "")
+
+  for id1 in input:
+    for id2 in input:
+      let diffIdx = stringDiff(id1, id2)
+
+      if diffIdx != -1:
+        var foo = toSeq(id1.items)
+        foo.delete(diffIdx, diffIdx)
+        return foo.map(proc(x: char): string = $x).mapIt(string, $it).join()
+
+echo "Checksum: ", partOne()
+echo "Common Letters: ", partTwo()
