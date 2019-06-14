a77160a4218d4e0ce763f829a1ff35d8e36c9e2b
Eric Solomon <eric@ericrsolomon.com>
2019-06-14 02:16:10 -0500

    updates

--- day_1/day_1.exs
+++ day_1/day_1.exs
@@ -10,10 +10,11 @@ end
 
 defmodule D1P2 do
   def run do
-    input = File.read!("input.txt")
-            |> String.trim()
-            |> String.split("\n")
-            |> Enum.map(&String.to_integer(&1))
+    input =
+      File.read!("input.txt")
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.map(&String.to_integer(&1))
 
     solve(input, [], 0)
   end
@@ -21,9 +22,11 @@ defmodule D1P2 do
   defp solve(freq), do: freq
   defp solve(list, seen, freq), do: solve(list, list, seen, freq)
   defp solve([], list, seen, freq), do: solve(list, seen, freq)
+
   defp solve([head | tail], list, seen, freq) do
     new_freq = freq + head
-    if (new_freq in seen) do
+
+    if new_freq in seen do
       solve(new_freq)
     else
       solve(tail, list, [new_freq | seen], new_freq)
